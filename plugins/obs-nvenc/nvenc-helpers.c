41a544ee2cb3cbc5ed22673fa8a145860eed4ca2
sep <sep@sep.gg>
2026-01-21 18:07:02 -0800

    look for obs_nvenc_test in PATH

--- plugins/obs-nvenc/nvenc-helpers.c
+++ plugins/obs-nvenc/nvenc-helpers.c
@@ -7,6 +7,37 @@
 #include <util/dstr.h>
 #include <util/pipe.h>
 
+#ifndef _WIN32
+#include <sys/stat.h>
+static char *find_executable_in_path(const char *name)
+{
+	const char *path_env = getenv("PATH");
+	if (!path_env || !name)
+		return NULL;
+
+	char *path_copy = bstrdup(path_env);
+	char *saveptr = NULL;
+	char *dir = strtok_r(path_copy, ":", &saveptr);
+
+	while (dir) {
+		struct dstr full_path = {0};
+		dstr_printf(&full_path, "%s/%s", dir, name);
+
+		struct stat st;
+		if (stat(full_path.array, &st) == 0 && (st.st_mode & S_IXUSR)) {
+			bfree(path_copy);
+			return full_path.array;
+		}
+
+		dstr_free(&full_path);
+		dir = strtok_r(NULL, ":", &saveptr);
+	}
+
+	bfree(path_copy);
+	return NULL;
+}
+#endif
+
 static void *nvenc_lib = NULL;
 static pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
 NV_ENCODE_API_FUNCTION_LIST nv = {NV_ENCODE_API_FUNCTION_LIST_VER};
@@ -298,6 +329,21 @@ static bool nvenc_check(void)
 	}
 #else
 	char *test_exe = os_get_executable_path_ptr("obs-nvenc-test");
+
+	bool exists = os_file_exists(test_exe);
+
+	if (!exists) {
+		blog(LOG_INFO, "Did not find NVENC test exe, fallback to PATH search");
+
+		char *path_exe = find_executable_in_path("obs-nvenc-test");
+		if (path_exe) {
+			blog(LOG_INFO, "Found obs-nvenc-test in PATH: %s", path_exe);
+			bfree(test_exe);
+			test_exe = path_exe;
+		} else {
+			blog(LOG_WARNING, "Could not find obs-nvenc-test in PATH");
+		}
+	}
 #endif
 	os_process_args_t *args;
 	struct dstr caps_str = {0};
